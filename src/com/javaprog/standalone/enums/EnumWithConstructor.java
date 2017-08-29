b3bb4ebf37e9ae47a0e54805923550ea77ab26df
abhishek.k <abhishek.k@sokrati.com>
2017-08-30 00:20:08 +0530

    adding test and explanation to EnumWithConstructor

--- src/com/javaprog/standalone/enums/EnumWithConstructor.java
+++ src/com/javaprog/standalone/enums/EnumWithConstructor.java
@@ -1,7 +1,10 @@
 package com.javaprog.standalone.enums;
 
+import java.util.Arrays;
+
 /* ref:
  * http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+ * https://stackoverflow.com/questions/604424/lookup-enum-by-string-value#2965252
  */
 public enum EnumWithConstructor
 {
@@ -27,6 +30,37 @@ public enum EnumWithConstructor
      * @return
      */
     public int getValue() {
+        // This method is equivalent of toString for String types
         return value;
+    }
+    
+    public String toString()
+    {
+        // returns the name of the enum
+        return name();
+    }
+    
+    /**
+     * Get Enum from value
+     * @return
+     */
+    public static EnumWithConstructor fromValue(int val)
+    {
+        for (EnumWithConstructor enumType : EnumWithConstructor.values())
+        {
+            if(enumType.getValue() == val)
+                return enumType;
+        }
+        return null;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println(EnumWithConstructor.valueOf("PENNY"));
+        System.out.println(Arrays.toString(EnumWithConstructor.values()));
+        System.out.println(EnumWithConstructor.valueOf(EnumWithConstructor.class, "PENNY"));
+        System.out.println(EnumWithConstructor.fromValue(5));
+        System.out.println(EnumWithConstructor.DIME.toString());
+        System.out.println(EnumWithConstructor.DIME.getValue());
     }
 }
